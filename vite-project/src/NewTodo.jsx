01e0ae0feb8ecf75c0c7634c45a29bc77b22f0d3
zel-bagh <zakaria.baghazaoui@gmail.com>
2023-05-20 23:05:42 +0100

    two components

---
+++ vite-project/src/NewTodo.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+export default function NewTodo(props){
+const [newitem, setnewitem] = useState("");
+
+function handleSubmit(e){
+
+    e.preventDefault();
+    props.onSubmit((currentTodos) => {
+      return [
+        ...currentTodos,
+        {id: crypto.randomUUID(), title: newitem, completed: false},
+    ]
+  });
+
+}
+
+return (
+    <form onSubmit={handleSubmit} className='form'>
+    <div className="new-item">
+    <label htmlFor='new-item'></label>
+    <input type= 'text' value={newitem} onChange={e => setnewitem(e.target.value)} id= 'new-item' ></input>
+    </div>
+    <button className='btn'> Add </button>
+    </form>
+)
+
+}
